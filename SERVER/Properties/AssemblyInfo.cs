3b07aac2484594c1263c584bfa22347772912b84
huytq1996 <nhoccendy@gmail.com>
2017-12-30 01:01:06 +0700

    sửa lại tên

--- SERVER/Properties/AssemblyInfo.cs
+++ SERVER/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TINH TONG 2 SO - ANTT")]
+[assembly: AssemblyTitle("SERVER")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TINH TONG 2 SO - ANTT")]
+[assembly: AssemblyProduct("SERVER")]
 [assembly: AssemblyCopyright("Copyright 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
